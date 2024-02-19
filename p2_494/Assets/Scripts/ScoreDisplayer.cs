cd74c5525581ebfbc2c8acf6454711079908c602
Nithisha Nantha Kumar <nithishananthakumar.tinie21@gmail.com>
2024-02-18 20:45:52 -0500

    Update ScoreDisplayer.cs

--- p2_494/Assets/Scripts/ScoreDisplayer.cs
+++ p2_494/Assets/Scripts/ScoreDisplayer.cs
@@ -17,17 +17,17 @@ public class ScoreDisplayer : MonoBehaviour
         if(e.new_score >= 60)
         {
             // Set to green
-            textComp.color = new Color(121, 213, 77);
+            textComp.color = new Color(121f / 255f, 213f / 255f, 77f / 255f, 1f);
         }
         else if (e.new_score >= 25)
         {
             // Set to yellow
-            textComp.color = new Color(253, 189, 26);
+            textComp.color = new Color(253f / 255f, 189f / 255f, 26f / 255f, 1f);
         }
         else
         {
             // Set to red
-            textComp.color = new Color(253, 27, 50);
+            textComp.color = new Color(253f / 255f, 27f / 255f, 50f / 255f, 1f);
         }
         if(e.new_score <= 10)
         {
